6ff232c6f4e8d6be744d5726ee25dd4163b39fe0
sashapoplavskiy <saspoplavskiy563@gmail.com>
2023-06-12 22:22:10 +0300

    Add code for slider which show one image

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,20 @@
 "use strict";
 
 const SubheaderContent = [
-    "Subheader 1",
-    "Subheader 2"
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!"
 ];
 
 const TextContent = [
-    "Text 1",
-    "Text 2"
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam.",
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam."
 ];
 
+const arrImage = [
+    "../img/slider/picture-one.png"
+];
+
+const blockSlider = document.querySelector("#slider"),
+      blockSliderImage = blockSlider.querySelector(".slider_image");
+
+blockSliderImage.style.background = 'url(../img/slider/picture-one.png) center center/cover no-repeat';
