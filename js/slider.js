36c06d9c8344ef95446ef2265e1b930f0a80a9cc
sashapoplavskiy <saspoplavskiy563@gmail.com>
2023-06-26 10:45:33 +0300

    create slider for web-site, but hir don	 text-content and img

--- js/slider.js
+++ js/slider.js
@@ -5,15 +5,29 @@ const SubheaderContent = [
     "2 ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!"
 ],
     TextContent = [
-    "Lorem 1 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam.",
-    "Lorem 2 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam."
-],
+        "Lorem 1 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam.",
+        "Lorem 2 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam."
+    ],
     arrImage = [
-    "../img/slider/picture-one.png",
-    "../img/slider/picture-two.png"
-];
+        "../img/slider/picture-one.png",
+        "../img/slider/picture-two.png"
+    ];
 
 const blockSlider = document.querySelector("#slider"),
-      blockSliderImage = blockSlider.querySelector(".slider_image");
+    blockSliderSubHeader = blockSlider.querySelector('.slider_content-subheader'),
+    blockSliderText = blockSlider.querySelector('.slider_content-text'),
+    blockSliderImage = blockSlider.querySelector(".slider_image");
 
-setInterval();
+let frame = 0;
+let slider = setInterval(function log() {
+
+    if (frame == arrImage.length)
+        frame = 0;
+    else {
+        blockSliderImage.style.background = 'url(' + arrImage[frame] + ') center center/cover no-repeat';
+        blockSliderSubHeader.innerHTML = SubheaderContent[frame];
+        blockSliderText.innerHTML = TextContent[frame];
+        frame++;
+    }
+
+}, 5000);
