7142f8c63de8a6fa60503db22d63bd5a5ee01bf3
sashapoplavskiy <saspoplavskiy563@gmail.com>
2023-06-26 10:13:56 +0300

    add new image

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,19 @@
 "use strict";
 
 const SubheaderContent = [
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!"
-];
-
-const TextContent = [
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam.",
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam."
-];
-
-const arrImage = [
-    "../img/slider/picture-one.png"
+    "1 ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!",
+    "2 ipsum dolor sit amet consectetur adipisicing elit. Aperiam, corrupti molestiae quo consectetur quasi magni! Ut libero vero pariatur consequatur. Illo id nihil officiis ipsam maxime eveniet. Soluta, tenetur reprehenderit!"
+],
+    TextContent = [
+    "Lorem 1 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam.",
+    "Lorem 2 dolor sit amet consectetur, adipisicing elit. Vitae ut molestiae perspiciatis sed ipsam laborum commodi culpa non repudiandae cupiditate magnam sequi pariatur excepturi repellat, est voluptatem officia aperiam quam."
+],
+    arrImage = [
+    "../img/slider/picture-one.png",
+    "../img/slider/picture-two.png"
 ];
 
 const blockSlider = document.querySelector("#slider"),
       blockSliderImage = blockSlider.querySelector(".slider_image");
 
-blockSliderImage.style.background = 'url(../img/slider/picture-one.png) center center/cover no-repeat';
+setInterval();
