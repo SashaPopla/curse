bed7df3dfbbf7bd079e57ca7981a2ef0658b8a54
Sasha Poplavskiy <saspoplavskiy563@gmail.com>
2023-07-13 21:59:44 +0300

    all project corect style write and class/tag name

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
 "use strict";
-
+/* first flider start*/
 const btnsGlobe = document.querySelectorAll(".globe"), 
     btnClose = document.querySelector(".modale_close");
 
 
 btnsGlobe.forEach((item)=>{
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
         let modaleWin = document.querySelector(".overlay");
-        modaleWin.style.display = 'block';
+        modaleWin.style.display = "block";
     });
 });
 
 btnClose.addEventListener('click', () => {
     let modaleWin = document.querySelector(".overlay");
-    modaleWin.style.display = 'none';
+    modaleWin.style.display = "none";
 }); 
+/* first flider end*/
 
 /* choose learning */
-const blockForBnts = document.querySelector('.choice_courses_btn'),
-    btns = blockForBnts.querySelectorAll('button'),
-    arrLessons = [ ...document.querySelectorAll('.courses_learne') ];
+const blockForBnts = document.querySelector(".choose_courses_btn"),
+    btns = blockForBnts.querySelectorAll("button"),
+    arrLessons = [ ...document.querySelectorAll(".courses_learne") ];
 
 console.log(btns);
 console.log(arrLessons);
 
 btns.forEach((item)=>{
-    item.addEventListener('click', ()=>{
-        if(item.innerText !== 'C#')
-            console.log('It`s not C#');
+    item.addEventListener("click", ()=>{
+        if(item.innerText !== "C#")
+            console.log("It`s not C#");
         console.log(item);
     });
 });
