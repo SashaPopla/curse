79b1a7e3c6a0689b03394846d8b674ec02342d5d
Sasha Poplavskiy <saspoplavskiy563@gmail.com>
2023-09-14 15:58:06 +0300

    created a choose for popular course lessons

--- js/script.js
+++ js/script.js
@@ -1,36 +1,57 @@
 "use strict";
-/* first flider start*/
-const btnsGlobe = document.querySelectorAll(".globe"), 
+document.addEventListener('DOMContentLoaded', ()=>{
+    /* first flider start*/
+    const btnsGlobe = document.querySelectorAll(".globe"), 
     btnClose = document.querySelector(".modale_close");
 
 
-btnsGlobe.forEach((item)=>{
-    item.addEventListener("click", () => {
+    btnsGlobe.forEach((item)=>{
+        item.addEventListener("click", () => {
+            let modaleWin = document.querySelector(".overlay");
+            modaleWin.style.display = "block";
+        });
+    });
+
+    btnClose.addEventListener('click', () => {
         let modaleWin = document.querySelector(".overlay");
-        modaleWin.style.display = "block";
+        modaleWin.style.display = "none";
+    }); 
+    /* first flider end*/
+
+    /* choose learning */
+    const blockForBnts = document.querySelector(".choose_courses_btn"),
+        btns = blockForBnts.querySelectorAll("button"),
+        arrLessons = document.querySelectorAll(".courses_learne");
+
+    function hideContentLearning(){
+        arrLessons.forEach((item)=>{
+            item.style.display = "none";
+        });
+
+        btns.forEach((item)=>{
+            item.classList.remove('active');
+        });
+    }
+
+    function showContentLearning(i = 0){
+        arrLessons[i].style.display = "block";
+        btns[i].classList.add('active');
+    }
+
+    hideContentLearning();
+    showContentLearning();
+
+    blockForBnts.addEventListener('click', (e)=>{
+        let target = e.target;
+
+        if(target && e.target.nodeName == 'BUTTON'){
+            btns.forEach((item, i)=>{
+                if(target == item){
+                    hideContentLearning();
+                    showContentLearning(i);
+                }
+            })
+        }
     });
+    /* end  */
 });
-
-btnClose.addEventListener('click', () => {
-    let modaleWin = document.querySelector(".overlay");
-    modaleWin.style.display = "none";
-}); 
-/* first flider end*/
-
-/* choose learning */
-const blockForBnts = document.querySelector(".choose_courses_btn"),
-    btns = blockForBnts.querySelectorAll("button"),
-    arrLessons = [ ...document.querySelectorAll(".courses_learne") ];
-
-console.log(btns);
-console.log(arrLessons);
-
-btns.forEach((item)=>{
-    item.addEventListener("click", (e)=>{
-        console.log(e.target);
-        /*if(item.innerText !== "C#")
-            console.log("It`s not C#");
-        console.log(item);*/
-    });
-});
-/* end  */
